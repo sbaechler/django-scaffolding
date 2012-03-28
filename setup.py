9896a2ec6a4c29f5fe5398652f2e1a11bef7f2cf
Matthias Kestenholz <mk@spinlock.ch>
2012-03-28 21:04:29 +0200

    Convert setup script to pure distutils, add everything important to MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
-from setuptools import find_packages
+import os
+import setuplib
+
+packages, package_data = setuplib.find_packages('scaffolding')
 
 setup(
     name='django-scaffolding',
     version='0.1.0',
     author='Simon Baechler',
     author_email='simon@stellanera.com',
-    packages=find_packages(),
-    url='https://github.com/sbaechler/django-scaffolding',
-    license='LICENSE',
+    packages=packages,
+    package_data=package_data,
+    url='https://github.com/sbaechler/django-scaffolding/',
+    license='LICENCE',
     description='Automatically generate reasonable database entries for your app',
-    long_description=open('README.rst').read(),
-    install_requires=[
-        "Django >= 1.2",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
     ],
-    zip_safe=False
 )
